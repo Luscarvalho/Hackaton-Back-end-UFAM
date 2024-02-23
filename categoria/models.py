3a753978e2b1a9730eadd221c5f18c64dc8fb4b6
Waleson Melo <walesonmelo23@gmail.com>
2024-02-23 09:49:42 -0400

    adiciona models

--- categoria/models.py
+++ categoria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    id_categoria = models.IntegerField(primary_key=True)
+    descricao = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
