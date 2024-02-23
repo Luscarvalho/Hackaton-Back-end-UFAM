b4d86289ed935a37c7086047ec36ea8e43146715
Waleson Melo <walesonmelo23@gmail.com>
2024-02-23 11:40:07 -0400

    ffff

--- reserva/models.py
+++ reserva/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 from quarto.models import Quarto
+from cliente.models import Cliente
 
 
 class Reserva(models.Model):
     dt_inicial = models.DateField("Data inicial")
     dt_final = models.DateField("Data final")
-    idclient = models.ForeignKey(User, verbose_name="Cliente", on_delete=models.CASCADE, null=True, blank=True)
+    idclient = models.ForeignKey(Cliente, verbose_name="Cliente", on_delete=models.CASCADE, null=True, blank=True)
     idquarto = models.ForeignKey(Quarto, verbose_name="Quarto", on_delete=models.CASCADE, null=True, blank=True)
