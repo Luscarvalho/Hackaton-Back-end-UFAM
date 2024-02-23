dbeb38775b1f62da345bc154cbf2adc69d4a483b
Waleson Melo <walesonmelo23@gmail.com>
2024-02-23 09:58:57 -0400

    adiciona models reserva

--- reserva/models.py
+++ reserva/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from quarto.models import Quarto
+
+
+class Reserva(models.Model):
+    dt_inicial = models.DateField()
+    dt_final = models.DateField()
+    idclient = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    idquarto = models.ForeignKey(Quarto, on_delete=models.CASCADE, null=True, blank=True)
